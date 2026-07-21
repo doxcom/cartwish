bca4dfbe823da6cd6a8dcd1dda1d6f9c9e2c8410
doxcom <aldoperez1319@gmail.com>
2026-07-21 15:10:32 -0600

    adding navbar with emoji and links

---
+++ cartwish/src/components/navbar/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import './Navbar.css'
+import rocket from '../../assets/rocket.png'
+import star from '../../assets/glowing-star.png'
+import idButton from '../../assets/id-button.png'
+import memo from '../../assets/memo.png'
+import order from '../../assets/package.png'
+import lock from '../../assets/locked.png'
+import LinkWithIcon from './LinkWithIcon'
+
+const Navbar = () => {
+  return (
+    <nav className='align_center navbar'>
+      <div className="align_center">
+        <h1 className='navbar_heading'> Cartwish</h1>
+         <form className='align_center navbar_form'>
+            <input type="text" className='navbar_search' placeholder='Search Products'/>
+            <button type="submit" className='search_button'> Search</button>
+         </form>
+      </div>
+      <div className="align_center navbar_links">
+        <LinkWithIcon title="Home" link="/" emoji={rocket} />
+         <LinkWithIcon title="Products" link="/products" emoji={star} />
+          <LinkWithIcon title="LogIn" link="/login" emoji={idButton} />
+           <LinkWithIcon title="SignUp" link="/signup" emoji={memo} />
+            <LinkWithIcon title="My Orders" link="/myorders" emoji={order} />
+             <LinkWithIcon title="Logout" link="/logout" emoji={lock} />
+       </div>
+    </nav>
+  )
+}
+
+export default Navbar
