3e0e8ce735123be636d6f21c1db20c2516e0d0af
doxcom <aldoperez1319@gmail.com>
2026-07-24 11:13:52 -0600

    adding single product image selection

--- cartwish/src/components/SingleProduct/SingleProductPage.jsx
+++ cartwish/src/components/SingleProduct/SingleProductPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './SingleProductPage.css'
 
@@ -9,26 +9,31 @@ import './SingleProductPage.css'
             "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime aliquid rerum a? Fugiat soluta facilis deleniti voluptatibus ab architecto dolores a, vero, beatae veniam error doloribus quia laudantium? Error fuga consequuntur quia accusantium? Consequatur modi laboriosam saepe culpa, ab atque.",
         price: 9.99,
         images: [
-            "https://via.placeholder.com/500x500?text=Product+Image+1",
-            "https://via.placeholder.com/500x500?text=Product+Image+2",
-            "https://via.placeholder.com/500x500?text=Product+Image+3",
-            "https://via.placeholder.com/500x500?text=Product+Image+4",
+            "https://placehold.co/500x500?text=Product+Image+1",
+            "https://placehold.co/500x500?text=Product+Image+2",
+            "https://placehold.co/500x500?text=Product+Image+3",
+            "https://placehold.co/500x500?text=Product+Image+4",
         ],
         stock: 10,
     };
 
 const SingleProductPage = () => {
+  const [selectedImage, setSelectedImage] = useState(0)
   return (
     <section className='align_center single_product'>
         <div className='align_center'>
             <div className='single_product_thumbnails'>
-                {
-                  product.images.map((image,index) => <img src={image} alt={product.title} 
-                  />)
-                }
+                {product.images.map((image,index) => (
+                    <img 
+                         src={image} 
+                         alt={product.title}
+                         className={selectedImage === index ? 'selected_image': ''} 
+                         onClick={() => setSelectedImage(index)}/>
+                ))}
             </div>
             
-            <img src={product.images[0]}
+            <img 
+                 src={product.images[selectedImage]}
                  alt={product.title}
                  className='single_product_display'
             />
