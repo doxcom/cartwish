2e40e60eaf0d9435aea9b3399b5efade46730fa0
doxcom <aldoperez1319@gmail.com>
2026-07-30 02:39:48 -0600

    adding useRef

--- cartwish/src/components/Authentication/LoginPage.jsx
+++ cartwish/src/components/Authentication/LoginPage.jsx
@@ -1,26 +1,45 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 import './LoginPage.css'
 
 const LoginPage = () => {
+  
+  const nameRef = useRef(null); 
+  const phoneRef = useRef(null); 
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const user = {
+        name: "",
+        phone: 0,
+    };
+    user.name = nameRef.current.value;
+    user.phone = parseInt(phoneRef.current.value);
+  }
+    
   return (
     <section className="align_center form_page">
-        <form className='authentication_form'>
+        <form className='authentication_form' onSubmit={handleSubmit}>
             <h2>Login Form</h2>
             <div className="form_inputs">
                 <div>
                     <label htmlFor='name'>Name</label>
                     <input type="text"
+                           ref={nameRef}
                            id="name" 
                            className='form_text_input' placeholder='Enter your name'/>
                 </div>
 
                   <div>
                     <label htmlFor='phone'>Phone Number</label>
-                    <input type="number"
+                    <input 
+                           type='number'
+                           ref={phoneRef}
                            id="phone"
                            className='form_text_input' placeholder='Enter your phone number'/>
                 </div>
+                
+
 
                 <button type="submit" className='search_button form_submit'>
                     Submit
