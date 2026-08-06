79235108c733e01cb784648878fe5ec80054f8a5
doxcom <aldoperez1319@gmail.com>
2026-08-05 20:21:32 -0600

    adding route to product part

--- cartwish/src/components/Products/ProductsList.jsx
+++ cartwish/src/components/Products/ProductsList.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProductsList.css'
 import ProductCard  from "./ProductCard";
+import apiClient from '../../utils/api-client';
 
 const ProducsList = () => {
+  const [products, setProducts] = useState([])
+  const [error, setError] = useState("")
+
+  useEffect(() => {
+     apiClient
+        .get("/products")
+        .then((res) => setProducts(res.data.products))
+        .catch((err) => setError(err.message));
+  },[])
+
   return (
     <section className='products_list_section'>
         <header className="align_center products_list_header">
@@ -17,14 +28,9 @@ const ProducsList = () => {
         </header>    
 
         <div className="products_list">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />           
+          {error && <em className="form_error">{error}</em>}  
+          {products.map(product => <ProductCard key={product._id}/>)} 
+
         </div>
     </section>    
   )
