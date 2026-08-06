ac2b94e1188e0c0d79afb4be47cc03e125317999
doxcom <aldoperez1319@gmail.com>
2026-08-05 20:47:54 -0600

    adding images into cards

--- cartwish/src/components/Products/ProductsList.jsx
+++ cartwish/src/components/Products/ProductsList.jsx
@@ -29,8 +29,18 @@ const ProducsList = () => {
 
         <div className="products_list">
           {error && <em className="form_error">{error}</em>}  
-          {products.map(product => <ProductCard key={product._id}/>)} 
-
+          {products.map((product) => (
+            <ProductCard 
+                key={product._id} 
+                id={product._id} 
+                image={product.images[0]} 
+                price={product.price} 
+                title={product.title}
+                rating={product.reviews.rate}
+                ratingCounts={product.reviews.counts}
+                stock={product.stock}
+                  />
+           ))} 
         </div>
     </section>    
   )
