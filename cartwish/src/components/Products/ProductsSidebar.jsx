71ca96976c1d5beb988de40be0eef88f23593923
doxcom <aldoperez1319@gmail.com>
2026-08-05 21:05:16 -0600

    adding fetch category

--- cartwish/src/components/Products/ProductsSidebar.jsx
+++ cartwish/src/components/Products/ProductsSidebar.jsx
@@ -1,20 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProductsSidebar.css'
 import LinkWithIcon from '../navbar/LinkWithIcon'
 import rocket from '../../assets/rocket.png'
+import apiClient from '../../utils/api-client'
 
 const ProductsSidebar = () => {
+  const [categories, setCategories] =useState([])
+  const [error, setError] = useState("")
+
+  useEffect(() =>{
+    apiClient
+         .get("/category")
+         .then((res) => setCategories(res.data))
+         .catch((err) => setError(err.message));
+  }, []);
+  
   return (
     <aside className='products_sidebar'>
         <h2>Category</h2>
 
         <div className='category_links'></div>
-          <LinkWithIcon 
-          title='Electronics'
-          link='products?category=electronics'
-          emoji={rocket}
+          {error && <em className="form_error">{error}</em>}
+          {categories.map(category => <LinkWithIcon
+          key={category._id} 
+          title={category.name}
+          link={`products?category=${category.name}`}
+          emoji={`http://localhost:5000/category/${category.image}`}
           sidebar={true}
-          />
+          />)}
+          
     </aside>
   )
 }
